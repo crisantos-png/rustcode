1d69a219612ea6a734b94fa21324bb1b1d178e86
Mr Anonymous <nihilisticand@god.hell>
2026-05-24 17:57:02 +0100

    nuggets are for me

--- svetlana/target/debug/svetlana.d
+++ svetlana/target/debug/svetlana.d
@@ -1,1 +1,1 @@
-/data/data/com.termux/files/home/rustcode/svetlana/target/debug/svetlana: /data/data/com.termux/files/home/rustcode/svetlana/src/main.rs
+/root/rustcode/svetlana/target/debug/svetlana: /root/rustcode/svetlana/src/main.rs
